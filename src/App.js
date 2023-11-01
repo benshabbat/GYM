b98c31f7c3df9e43694234275d4716928ddc4003
benshabbat <benshabbat27@gmail.com>
2023-11-01 23:00:19 +0200

    starting routes and build comps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import "./app.css"
+import React from "react";
+import "./app.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Box width="400px">
+        
+        <Routes>
+          
+        </Routes>
+        <div>App</div>
+      </Box>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
