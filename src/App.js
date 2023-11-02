81b75e88dc1f67f339ac2b700d6b43d5f89e37f0
benshabbat <benshabbat27@gmail.com>
2023-11-02 20:41:14 +0200

    browser router to index

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from "react";
 import "./app.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import ExerciseDetail from "./pages/ExerciseDetail";
 const App = () => {
   return (
-    <BrowserRouter>
-      <Box width="400px">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/execise/:id" element={<ExerciseDetail />} />
-        </Routes>
-        <Footer/>
-      </Box>
-    </BrowserRouter>
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/execise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   );
 };
 
