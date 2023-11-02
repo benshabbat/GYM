d715ce62c197ca60aa4ce156a372971fa243d16c
benshabbat <benshabbat27@gmail.com>
2023-11-02 20:27:29 +0200

    added two routs home and exercises

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import "./app.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
-
+import Navbar from "./components/NavBar";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
 const App = () => {
   return (
     <BrowserRouter>
       <Box width="400px">
-        
+
         <Routes>
-          
+          <Route path="/" element={<Home/>}/>
+          <Route path="/execise/:id" element={<ExerciseDetail/>}/>
         </Routes>
         <div>App</div>
       </Box>
