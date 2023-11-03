871f7e6f08e824697de0e12f521bcf619ce3ecc9
benshabbat <benshabbat27@gmail.com>
2023-11-03 10:19:34 +0200

    done navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,16 @@ import Logo from "../assets/images/logo.png";
 
 const Navbar = () => {
   return (
-    <Stack>
+    <Stack
+      direction="row"
+      justifyContent="space-around"
+      sx={{
+        gap: { sm: "122px", xs: "40px" },
+        mt: { sm: "32px", xs: "20px" },
+        justifyContent: "none",
+      }}
+      px="20px"
+    >
       <Link to="/">
         <img
           src={Logo}
