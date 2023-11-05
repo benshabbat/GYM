fe932dbc1eb5402805d63a4afc476a34594e269d
benshabbat <benshabbat27@gmail.com>
2023-11-05 14:28:55 +0200

    starting hero banner

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import { Box,Stack,Typography } from "@mui/material";
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <Box>
+      <Typography color="#ff2625" fontWeight="600">
+        Gym Club
+      </Typography>
+    </Box>
   )
 }
 
