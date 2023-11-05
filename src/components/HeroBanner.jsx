c50dee1823de77c6d404f0cde3a6c1ee74074a0c
benshabbat <benshabbat27@gmail.com>
2023-11-05 22:54:07 +0200

    mor paragraph and mor css on them

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -13,8 +13,16 @@ const HeroBanner = () => {
       <Typography color="#ff2625" fontWeight="600" fontSize="26px">
         Gym Club
       </Typography>
-      <Typography>
+      <Typography
+        fontWeight={700}
+        xs={{
+          fontSize: { lg: "44px", xs: "40px" },
+        }}
+      >
         Welcome to the our Gym Club :)
+      </Typography>
+      <Typography fontSize="24px" lineHeight="35px">
+        You can looking on our Exercises section
       </Typography>
     </Box>
   );
