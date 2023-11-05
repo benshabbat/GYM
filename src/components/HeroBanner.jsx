3d5e0a04521b38f17e86dbb8b687260eb7e6e903
benshabbat <benshabbat27@gmail.com>
2023-11-05 22:49:06 +0200

    som css on box and more one paragraph

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import { Box,Stack,Typography } from "@mui/material";
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
 const HeroBanner = () => {
   return (
-    <Box>
-      <Typography color="#ff2625" fontWeight="600">
+    <Box
+      sx={{
+        mt: { lg: "212px", xs: "70px" },
+        ml: { sm: "50px" },
+      }}
+      position="relative"
+      p="20px"
+    >
+      <Typography color="#ff2625" fontWeight="600" fontSize="26px">
         Gym Club
       </Typography>
+      <Typography>
+        Welcome to the our Gym Club :)
+      </Typography>
     </Box>
-  )
-}
+  );
+};
 
-export default HeroBanner
+export default HeroBanner;
