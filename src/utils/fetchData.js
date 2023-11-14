a9fd4add4c3d0ba59721ff7cf205d5fe98c7e140
benshabbat <benshabbat27@gmail.com>
2023-11-14 19:56:29 +0200

    create env with data

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,3 +1,16 @@
+export const execisesOptions = {
+    method: 'GET',
+    url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/back',
+    params: {limit: '10'},
+    headers: {
+      'X-RapidAPI-Key':process.env.RAPID_API_KEY ,
+      'X-RapidAPI-Host': process.env.RAPID_API_HOST
+    }
+  };
+
+
+
+
 export const fetchData = async (url, options) => {
   const res = await fetch(url, options);
   const data = await res.json();
